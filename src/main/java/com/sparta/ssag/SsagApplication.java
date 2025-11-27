6f19e758af3a7ac1d3fcc08fdf05a2d1a1ced2f2
kangkigs <dlflq11@gmail.com>
2025-11-27 13:03:40 +0900

    [init] init commit

---
+++ src/main/java/com/sparta/ssag/SsagApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.ssag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/ssag/SsagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.ssag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

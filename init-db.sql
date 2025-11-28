88e3d673ce396c4e7c415129285f803ad16ed93e
narahim <79778428+skfkgla@users.noreply.github.com>
2025-11-28 16:23:35 +0900

    [fix] db 생성 쿼리 변경 (#16)

--- init-db.sql
+++ init-db.sql
@@ -1,4 +1,7 @@
--- product_service
-CREATE SCHEMA IF NOT EXISTS product_service_db;
--- recommend_service
-CREATE SCHEMA IF NOT EXISTS recommend_service_db;
+CREATE DATABASE product_service_db;
+CREATE DATABASE recommend_service_db;
+
+\c recommend_service_db
+
+-- pgvector 확장 설치
+CREATE EXTENSION IF NOT EXISTS vector;

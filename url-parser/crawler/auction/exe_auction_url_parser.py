deeeb1e6e89e4442c3c694eaa66f14edf47affa4
minju26 <mjalswn26@gmail.com>
2025-11-28 15:21:14 +0900

    [feat] 상세 페이지 URL Kafka로 발행
    - Kafka Producer 설정, publish
    - logging 설정 추가

--- url-parser/crawler/auction/exe_auction_url_parser.py
+++ url-parser/crawler/auction/exe_auction_url_parser.py
@@ -1,6 +1,18 @@
+from kafka import KafkaProducer
 from sqlalchemy import create_engine, MetaData, Table, select
 from .auction_url_parser import AuctionUrlParser
 from .config import *
+import json
+import logging
+
+# 로깅 설정
+logging.basicConfig(
+    level=logging.INFO,  # INFO 이상 레벨만 출력
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler()  # 콘솔 출력
+    ]
+)
 
 # DB 연결
 engine = create_engine(DATABASE_URL)
@@ -13,20 +25,25 @@ p_main_product = Table(
     autoload_with=engine
 )
 
-# DB에서 키워드 가져오기
 with engine.connect() as conn:
-    stmt = select(p_main_product)
+    stmt = select(p_main_product.c.id, p_main_product.c.name)
     rows = conn.execute(stmt).mappings().all()
 
-keyword_list = [row["name"] for row in rows]
+# Kafka Producer 설정
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092',
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
 
-# 검색 수행
+# 검색 수행 후 링크 publish
 parser = AuctionUrlParser()
-for keyword in keyword_list:
+for row in rows:
+    product_id = str(row['id'])
+    keyword = row['name']
     links = parser.get_product_urls(keyword)
-    print(f"{keyword} 검색 완료: {len(links)}개 링크")
-    for link in links:
-        print(link)
-    print("-" * 50)
+    logging.info(f"{keyword} 검색 완료: {len(links)}개 링크")
+    producer.send('auction_links', {'id': product_id, 'urls': links})
 
 parser.quit()
+producer.flush()
+producer.close()

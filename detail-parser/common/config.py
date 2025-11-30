a7007cef4461fe23549e252cc820fb1e4d39fcee
minju26 <mjalswn26@gmail.com>
2025-11-30 17:36:42 +0900

    [chore] common 패키지 및 유틸리티 추가

---
+++ detail-parser/common/config.py
@@ -0,0 +1,7 @@
+MAX_LINKS = 5
+DATABASE_URL = "postgresql+psycopg2://postgres:qwer1234!@localhost:5432/postgres"
+TARGET_CUPANG_URL = "https://www.coupang.com/"
+TARGET_NAVER_STORE_URL = "https://shopping.naver.com/ns/home"
+TARGET_AUCTION_URL="https://www.auction.co.kr"
+TARGET_11ST_URL="https://search.11st.co.kr"
+TARGET_GMARKET_URL="https://www.gmarket.co.kr"

b26e4ef76ac959f3a92546764dd8cd31dffc6b95
minju26 <mjalswn26@gmail.com>
2025-11-30 23:10:21 +0900

    [refactor] 실행 코드 main 가드 내부로 이동

--- detail-parser/crawler/auction/exe_auction_detail_parser.py
+++ detail-parser/crawler/auction/exe_auction_detail_parser.py
@@ -12,18 +12,20 @@ consumer = create_consumer(
 # Logging 설정
 setup_logger()
 
-parser = AuctionDetailParser()
-for url_info in consumer:
-    # url 정보
-    logging.info("url_info: {}".format(url_info.value))
-    product_id = url_info.value['id']
-    urls = url_info.value['urls']
+if __name__ == "__main__":
+    parser = AuctionDetailParser()
 
-    # 대표 상품에 대한 상세 상품 정보 수집
-    logging.info(f"product_id: {product_id}")
-    for url in urls:
-        info = parser.get_product_details(url)
-        logging.info(f"info: {info}")
+    for url_info in consumer:
+        # url 정보
+        logging.info("url_info: {}".format(url_info.value))
+        product_id = url_info.value['id']
+        urls = url_info.value['urls']
 
-    # TODO: kafka publish( -> product-service에서 consume)
-parser.quit()
+        # 대표 상품에 대한 상세 상품 정보 수집
+        logging.info(f"product_id: {product_id}")
+        for url in urls:
+            info = parser.get_product_details(url)
+            logging.info(f"info: {info}")
+
+        # TODO: kafka publish( -> product-service에서 consume)
+    parser.quit()

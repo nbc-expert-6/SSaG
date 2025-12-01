bb5cdbfc958fc084d9bc79590ef4e3f0807b750b
brian110326 <brian110326@gmail.com>
2025-12-01 15:45:40 +0900

    [refactor] : Kafka bootstrap 서버 주소 하드코딩 제거 및 @Value로 외부 설정 적용

--- recommend-service/src/main/java/com/sparta/recommendservice/domain/product_vector/infrastructure/config/KafkaConsumerConfig.java
+++ recommend-service/src/main/java/com/sparta/recommendservice/domain/product_vector/infrastructure/config/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,13 +18,16 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 @Configuration
 public class KafkaConsumerConfig {
 
+	@Value("${spring.kafka.consumer.bootstrap-servers}")
+	private String bootstrapServers;
+
 	@Bean
 	public ConsumerFactory<String, Object> consumerFactory() {
 		JsonDeserializer<Object> deserializer = new JsonDeserializer<>(Object.class);
 		deserializer.addTrustedPackages("*");
 
 		Map<String, Object> props = new HashMap<>();
-		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, "recommend-group");
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);

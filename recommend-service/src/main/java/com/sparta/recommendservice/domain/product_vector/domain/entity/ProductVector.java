83700eb79d0322a4b623cdf10c2556470921b721
brian110326 <brian110326@gmail.com>
2025-12-01 11:02:23 +0900

    [refactor] : ProductVector Converter 추가(직렬화 문제 해결)

--- recommend-service/src/main/java/com/sparta/recommendservice/domain/product_vector/domain/entity/ProductVector.java
+++ recommend-service/src/main/java/com/sparta/recommendservice/domain/product_vector/domain/entity/ProductVector.java
@@ -2,9 +2,15 @@ package com.sparta.recommendservice.domain.product_vector.domain.entity;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import com.fasterxml.jackson.databind.JsonNode;
 import com.pgvector.PGvector;
+import com.sparta.recommendservice.domain.product_vector.domain.convert.PGVectorConverter;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -24,49 +30,19 @@ public class ProductVector {
 	private UUID productId;
 
 	@Column(name = "embedding", columnDefinition = "public.vector(128)", nullable = false)
+	@Convert(converter = PGVectorConverter.class)
 	private PGvector embedding;
 
-	@Column(name = "metadta", columnDefinition = "jsonb")
-	private String metadata;
+	@Column(name = "metadata", columnDefinition = "jsonb")
+	@JdbcTypeCode(SqlTypes.JSON)
+	private JsonNode metadata;
 
 	@Builder
-	public ProductVector(UUID productId, PGvector embedding, String metadata) {
+	public ProductVector(UUID productId, PGvector embedding, JsonNode metadata) {
 		this.productId = productId;
 		this.embedding = embedding;
 		this.metadata = metadata;
 	}
 
-	/**
-	 * 다른 ProductVector와 cosine distance 계산
-	 * recommend()에서 1.0 - cosineDistance(...) 형태로 사용됨
-	 *
-	 * @param other 비교 대상 ProductVector
-	 * @return cosine similarity (0~1)
-	 */
-	public double cosineDistance(ProductVector other) {
-		float[] vec1 = this.embedding.toArray();
-		float[] vec2 = other.embedding.toArray();
-
-		if (vec1.length != vec2.length) {
-			throw new IllegalStateException("Vector lengths do not match.");
-		}
-
-		double dotProduct = 0.0;
-		double normA = 0.0;
-		double normB = 0.0;
-
-		for (int i = 0; i < vec1.length; i++) {
-			dotProduct += vec1[i] * vec2[i];
-			normA += vec1[i] * vec1[i];
-			normB += vec2[i] * vec2[i];
-		}
-
-		if (normA == 0.0 || normB == 0.0) {
-			return 0.0;
-		}
-
-		return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
-
-	}
-
 }
+

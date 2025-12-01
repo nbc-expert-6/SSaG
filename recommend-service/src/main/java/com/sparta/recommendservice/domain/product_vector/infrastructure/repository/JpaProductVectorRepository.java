544831beded79798556e9d2c6240e4131e0c4829
brian110326 <brian110326@gmail.com>
2025-12-01 11:01:58 +0900

    [feat] : ProductVector Repository 작성

--- recommend-service/src/main/java/com/sparta/recommendservice/domain/product_vector/infrastructure/repository/JpaProductVectorRepository.java
+++ recommend-service/src/main/java/com/sparta/recommendservice/domain/product_vector/infrastructure/repository/JpaProductVectorRepository.java
@@ -1,22 +1,27 @@
 package com.sparta.recommendservice.domain.product_vector.infrastructure.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import com.pgvector.PGvector;
 import com.sparta.recommendservice.domain.product_vector.domain.entity.ProductVector;
 
 public interface JpaProductVectorRepository extends JpaRepository<ProductVector, UUID> {
 
 	@Query(value = """
-		SELECT product_id, embedding, metadata FROM p_product_vector 
-				ORDER BY embedding <=> :targetEmbedding LIMIT :candidateSize
+		SELECT product_id, embedding, metadata FROM p_product_vector
+		ORDER BY embedding <=> CAST(?1 AS vector)
+		LIMIT ?2
 		""", nativeQuery = true)
-	List<ProductVector> findTopKByEmbedding(@Param("targetEmbedding") PGvector targetEmbedding,
-		@Param("candidateSize") int candidateSize);
+	List<Object[]> findTopKByEmbedding(String targetEmbedding, int candidateSize);
+
+	Optional<ProductVector> findByProductId(UUID productId);
+
+	@Query(value = "SELECT p.product_id FROM p_product_vector p", nativeQuery = true)
+	List<UUID> findAllProductId();
 
 }
+

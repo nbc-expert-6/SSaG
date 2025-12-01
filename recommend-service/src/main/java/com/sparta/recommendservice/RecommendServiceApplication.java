61414a0a8bdbf96b4c8154177ef2df8576bf1be5
brian110326 <brian110326@gmail.com>
2025-12-01 15:45:23 +0900

    [feat] : Product FeignClient 구현

--- recommend-service/src/main/java/com/sparta/recommendservice/RecommendServiceApplication.java
+++ recommend-service/src/main/java/com/sparta/recommendservice/RecommendServiceApplication.java
@@ -2,8 +2,10 @@ package com.sparta.recommendservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class RecommendServiceApplication {
 
 	public static void main(String[] args) {
---
+++ recommend-service/src/main/java/com/sparta/recommendservice/domain/product_vector/infrastructure/external/dto/ProductInfoFeignClientResponse.java
@@ -0,0 +1,12 @@
+package com.sparta.recommendservice.domain.product_vector.infrastructure.external.dto;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ProductInfoFeignClientResponse(
+	UUID productId,
+	String brand,
+	UUID categoryMediumId,
+	BigDecimal price
+) {
+}

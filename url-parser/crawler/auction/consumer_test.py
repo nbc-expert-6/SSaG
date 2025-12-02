bceb16d7b24ef8a1f9ec23bfab8248a9dfc7c0c1
minju26 <mjalswn26@gmail.com>
2025-12-02 22:28:05 +0900

    [chore] kafka consumer 테스트 파일 삭제

--- url-parser/crawler/auction/consumer_test.py
+++
@@ -1,9 +0,0 @@
-from ...common.kafka_utils import create_consumer
-
-consumer = create_consumer(
-    topic='auction_links',
-    group_id='test-group'
-)
-
-for message in consumer:
-    print(message.value)

ca90201290fdfbf5325a54687a4ba8cfd4608d2d
minju26 <mjalswn26@gmail.com>
2025-12-02 14:57:38 +0900

    [feat] 브랜드 정보 파싱 추가

--- detail-parser/crawler/elevenst/elevenst_detail_parser.py
+++ detail-parser/crawler/elevenst/elevenst_detail_parser.py
@@ -5,6 +5,7 @@ import undetected_chromedriver as uc
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from ..detail_parser import DetailParser
 from ...common.logging_utils import setup_logger
 
@@ -31,8 +32,26 @@ class ElevenStDetailParser(DetailParser):
     def get_product_info(self) -> dict:
         driver = self.driver
 
-        # 브랜드(임시)
-        brand = ""
+        # 브랜드
+        try:
+            # 상품 정보 탭으로 이동
+            review_tab = self.wait.until(ec.presence_of_element_located((By.ID, "tabMenuDetail1")))
+            self.driver.execute_script("arguments[0].scrollIntoView({block:'center'});", review_tab)
+            time.sleep(0.5)
+
+            self.wait.until(ec.element_to_be_clickable((By.ID, "tabMenuDetail1")))
+            review_tab.click()
+            time.sleep(1.2)
+            logging.info("[get_product_info] 상품 정보 탭 클릭 완료")
+
+            # 브랜드 정보 파싱
+            brand_el = driver.find_element(By.XPATH,
+                    "//table[contains(@class,'prdc_detail_table')]//th[contains(text(),'브랜드')]/following-sibling::td")
+            brand = brand_el.text.strip()
+        except Exception as e:
+            logging.exception(f"[get_product_info] 브랜드 파싱 실패: {e}")
+            brand = ""
+
         # 이미지
         img = driver.find_element(By.CSS_SELECTOR, "div.img_full img").get_attribute('src')
         # 판매자 정보

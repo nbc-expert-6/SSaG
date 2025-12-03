c80e6677925d2b7587a6129de8b10c1ddd79b1f9
kangkigs <dlflq11@gmail.com>
2025-12-03 21:52:30 +0900

    [chore] 일부 오타 수정

--- detail-parser/crawler/coupang/main.py
+++ detail-parser/crawler/coupang/main.py
@@ -29,8 +29,6 @@ if __name__ == "__main__":
         for url in urls:
             product_details = parser.get_product_details(url)
             product_details["main_product_id"] = main_product_id
-            product_details["platform"] = "auction"
-            product_details["sale_link"] = url
 
             producer.send('product-details', product_details)
             logging.info(f"[publish] product-details: {product_details}")

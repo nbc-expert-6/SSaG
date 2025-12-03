4e3fda318acddb1282ee7fb0ecc101181d7593d2
kangkigs <dlflq11@gmail.com>
2025-12-03 21:56:28 +0900

    [chore] 의존성 추가 및 일부경로 절대경로로 변환

--- review-parser/crawler/auction/main.py
+++ review-parser/crawler/auction/main.py
@@ -1,7 +1,7 @@
-from .auction_review_parser import AuctionReviewParser
-from ...common.kafka_utils import create_consumer
-from ...common.kafka_utils import create_producer
-from ...common.logging_utils import setup_logger
+from crawler.auction.auction_review_parser import AuctionReviewParser
+from common.kafka_utils import create_consumer
+from common.kafka_utils import create_producer
+from common.logging_utils import setup_logger
 import logging
 
 # Kafka Consumer 설정

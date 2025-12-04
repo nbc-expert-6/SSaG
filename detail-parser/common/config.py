2964255aa5bb5d2d9c07af9d8f93a3d4855659e6
minju26 <mjalswn26@gmail.com>
2025-12-04 18:41:04 +0900

    [chore] 사용하지 않는 상수 삭제

--- detail-parser/common/config.py
+++ detail-parser/common/config.py
@@ -8,11 +8,3 @@ load_dotenv()
 KAFKA_BOOTSTRAP = os.getenv("KAFKA_BOOTSTRAP")
 CHROME_BINARY = os.getenv("CHROME_BINARY")
 CHROMEDRIVER_PATH = os.getenv("CHROMEDRIVER_PATH")
-
-MAX_LINKS = 5
-
-TARGET_CUPANG_URL = "https://www.coupang.com/"
-TARGET_NAVER_STORE_URL = "https://shopping.naver.com/ns/home"
-TARGET_AUCTION_URL="https://www.auction.co.kr"
-TARGET_11ST_URL="https://search.11st.co.kr"
-TARGET_GMARKET_URL="https://www.gmarket.co.kr"

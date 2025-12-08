d701088e11f0b6d1c4673108f58c569168e8b149
brian110326 <brian110326@gmail.com>
2025-12-08 16:46:19 +0900

    [feat] : analysis-service docker 작성

--- analysis-service/src/main/java/com/sparta/analysisservice/domain/product_analysis/infrastructure/config/KafkaConsumerConfig.java
+++ analysis-service/src/main/java/com/sparta/analysisservice/domain/product_analysis/infrastructure/config/KafkaConsumerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,8 +18,8 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 @Configuration
 public class KafkaConsumerConfig {
 
-	/*@Value("${spring.kafka.consumer.bootstrap-servers}")
-	private String bootstrapServers;*/
+	@Value("${spring.kafka.consumer.bootstrap-servers}")
+	private String bootstrapServers;
 
 	@Bean
 	public ConsumerFactory<String, Object> consumerFactory() {
@@ -26,7 +27,7 @@ public class KafkaConsumerConfig {
 		deserializer.addTrustedPackages("*");
 
 		Map<String, Object> props = new HashMap<>();
-		//props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, "click-group");
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);

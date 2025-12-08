f45c8e81eb70f9015a5d65c16d3b704b8b4864d5
brian110326 <brian110326@gmail.com>
2025-12-08 16:10:39 +0900

    [feat] : docker 파일 작성

--- analysis-service/src/main/java/com/sparta/analysisservice/domain/product_analysis/infrastructure/config/KafkaProducerConfig.java
+++ analysis-service/src/main/java/com/sparta/analysisservice/domain/product_analysis/infrastructure/config/KafkaProducerConfig.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,13 +15,13 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @Configuration
 public class KafkaProducerConfig {
 
-	@Value("${spring.kafka.producer.bootstrap-servers}")
-	private String bootstrapServers;
+	/*@Value("${spring.kafka.producer.bootstrap-servers}")
+	private String bootstrapServers;*/
 
 	@Bean
 	public ProducerFactory<String, Object> producerFactory() {
 		Map<String, Object> configs = new HashMap<>();
-		configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		//configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
 		configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 

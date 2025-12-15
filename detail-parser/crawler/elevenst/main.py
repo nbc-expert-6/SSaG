89ceacc46ef603b1feb62de14aaab71507a30b99
minju26 <mjalswn26@gmail.com>
2025-12-16 01:15:09 +0900

    [feat] detail parser 메트릭 수집

--- detail-parser/crawler/elevenst/main.py
+++ detail-parser/crawler/elevenst/main.py
@@ -3,10 +3,14 @@ import time
 
 from common.kafka_utils import create_consumer, create_producer
 from common.logging_utils import setup_logger
+from common.monitoring.metrics import (CRAWL_EXCEPTION_COUNT, CRAWL_LATENCY,
+                                       CRAWL_TRIAL_COUNT, KAFKA_PUBLISH_COUNT,
+                                       KAFKA_PUBLISH_LATENCY)
+from common.monitoring.metrics_server import start_metrics_server
 from common.platform import Platform
 from crawler.elevenst.elevenst_detail_parser import ElevenStDetailParser
 
-# [PERF] 전체 코드 실행 start
+# 시작 시간
 total_start = time.perf_counter()
 
 # Kafka Consumer 설정
@@ -22,6 +26,8 @@ producer = create_producer()
 setup_logger()
 
 if __name__ == "__main__":
+    start_metrics_server()
+
     parser = ElevenStDetailParser()
 
     for url_info in consumer:
@@ -34,21 +40,34 @@ if __name__ == "__main__":
         for url in urls:
             # [PERF] detail 파싱 start
             parser_start = time.perf_counter()
+            CRAWL_TRIAL_COUNT.labels(platform="elevenst").inc()
 
-            product_details = parser.get_product_details(url)
-            product_details["main_product_id"] = main_product_id
-            product_details["platform"] = Platform.ELEVENST.value
+            try:
+                product_details = parser.get_product_details(url)
+                product_details["main_product_id"] = main_product_id
+                product_details["platform"] = Platform.ELEVENST.value
 
-            producer.send('product-details', product_details)
+                # 파싱 종료
+                parser_end = time.perf_counter()
+                CRAWL_LATENCY.labels(platform="elevenst").observe(parser_end - parser_start)
+                logging.info(f"[PERF] ===== parsing time: {parser_end - parser_start:.4f}s =====")
 
-            # [PERF] detail 파싱 end
-            parser_end = time.perf_counter()
-            logging.info(f"[PERF] ===== parsing time: {parser_end - parser_start:.4f}s =====")
+                kafka_start = time.perf_counter()
+                producer.send('product-details', product_details)
+                kafka_end = time.perf_counter()
+                KAFKA_PUBLISH_LATENCY.observe(kafka_end - kafka_start)
+                KAFKA_PUBLISH_COUNT.inc()
 
-            logging.info(f"[publish] product-details: {product_details}")
+                logging.info(f"[publish] product-details: {product_details}")
+
+            except Exception as e:
+                logging.error(
+                    f"처리 실패. main_product_id={main_product_id}, url={url}, error={e}",
+                    exc_info=True
+                )
+                CRAWL_EXCEPTION_COUNT.labels(platform="elevenst").inc()
 
     parser.quit()
 
-# [PERF] 전체 코드 실행 end
-total_end = time.perf_counter()
-logging.info(f"[PERF] ===== total time: {total_end - total_start:.4f}s =====")
+    total_end = time.perf_counter()
+    logging.info(f"[PERF] ===== total time: {total_end - total_start:.4f}s =====")
--- detail-parser/crawler/gmarket/main.py
+++ detail-parser/crawler/gmarket/main.py
@@ -1,11 +1,14 @@
+import logging
 import time
 
+from common.kafka_utils import create_consumer, create_producer
+from common.logging_utils import setup_logger
+from common.monitoring.metrics import (CRAWL_EXCEPTION_COUNT, CRAWL_LATENCY,
+                                       CRAWL_TRIAL_COUNT, KAFKA_PUBLISH_COUNT,
+                                       KAFKA_PUBLISH_LATENCY)
+from common.monitoring.metrics_server import start_metrics_server
 from common.platform import Platform
 from crawler.gmarket.gmarket_detail_parser import GmarketDetailParser
-from common.kafka_utils import create_consumer
-from common.kafka_utils import create_producer
-from common.logging_utils import setup_logger
-import logging
 
 # 시작 시간
 total_start = time.perf_counter()
@@ -23,6 +26,8 @@ producer = create_producer()
 setup_logger()
 
 if __name__ == "__main__":
+    start_metrics_server()
+
     parser = GmarketDetailParser()
 
     for url_info in consumer:
@@ -35,26 +40,43 @@ if __name__ == "__main__":
         for url in urls:
             # 파싱 시작
             parser_start = time.perf_counter()
+            CRAWL_TRIAL_COUNT.labels(platform="gmarket").inc()
 
-            product_details = parser.get_product_details(url)
-            product_details["main_product_id"] = main_product_id
-            product_details["platform"] = Platform.GMARKET.value
+            try:
+                product_details = parser.get_product_details(url)
+                product_details["main_product_id"] = main_product_id
+                product_details["platform"] = Platform.GMARKET.value
 
-            # 파싱 결과 유효성 최소 검증
-            if(product_details["price"] == None):
-                logging.info(f"{main_product_id}의 상품 상세 파싱 실패")
+                # 파싱 결과 유효성 최소 검증
+                if(product_details["price"] == None):
+                    logging.info(f"{main_product_id}의 상품 상세 파싱 실패")
+                    parser_end = time.perf_counter()
+                    CRAWL_LATENCY.labels(platform="gmarket").observe(parser_end - parser_start)
+                    logging.info(f"[PERF] ===== parsing time: {parser_end - parser_start:.4f}s =====")
+                    continue
+
+                # 파싱 종료
                 parser_end = time.perf_counter()
+                CRAWL_LATENCY.labels(platform="gmarket").observe(parser_end - parser_start)
                 logging.info(f"[PERF] ===== parsing time: {parser_end - parser_start:.4f}s =====")
-                continue
 
-            producer.send('product-details', product_details)
+                # kafka publish
+                kafka_start = time.perf_counter()
+                producer.send('product-details', product_details)
+                kafka_end = time.perf_counter()
+                KAFKA_PUBLISH_LATENCY.observe(kafka_end - kafka_start)
+                KAFKA_PUBLISH_COUNT.inc()
 
-            # 파싱 종료
-            parser_end = time.perf_counter()
-            logging.info(f"[PERF] ===== parsing time: {parser_end - parser_start:.4f}s =====")
-            logging.info(f"[publish] product-details: {product_details}")
+                logging.info(f"[publish] product-details: {product_details}")
+
+            except Exception as e:
+                logging.error(
+                    f"처리 실패. main_product_id={main_product_id}, url={url}, error={e}",
+                    exc_info=True
+                )
+                CRAWL_EXCEPTION_COUNT.labels(platform="gmarket").inc()
 
     parser.quit()
 
-total_end = time.perf_counter()
-logging.info(f"[PERF] ===== total time: {total_end - total_start:.4f}s =====")
+    total_end = time.perf_counter()
+    logging.info(f"[PERF] ===== total time: {total_end - total_start:.4f}s =====")

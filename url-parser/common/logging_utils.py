b5ecfbfa476b3294edee663e693c5ced441a48d8
minju26 <mjalswn26@gmail.com>
2025-12-16 19:19:08 +0900

    [chore] 로그 모니터링 초기 설정
    - struclog 라이브러리 추가
    - promtail.yml 작성
    - logger.py 설정
    - 기존 logging 세팅함수 이름 변경

--- url-parser/common/logging_utils.py
+++ url-parser/common/logging_utils.py
@@ -1,7 +1,8 @@
 import logging
 
+
 # Logging 설정
-def setup_logger():
+def setup_local_logger():
     logging.basicConfig(
         level=logging.INFO,  # INFO 이상 레벨만 출력
         format="%(asctime)s [%(levelname)s] %(message)s",
